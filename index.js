0de283ba727bec404a25e5530cc3d9bafb291e9d
bejebus <gogs@fake.local>
2018-04-14 20:14:57 +0100

    removed dependencies; now returns any combination of any number of words

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 'use strict';
 const fs = require('fs');
-const uniqueRandomArray = require('unique-random-array');
 const wordListPath = require('word-list');
 
-module.exports = uniqueRandomArray(fs.readFileSync(wordListPath, 'utf8').split('\n'));
+let list = [];
+
+function getList() {
+    return fs.readFileSync(wordListPath, 'utf8').split(/\r\n/m);
+}
+
+function rando(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+module.exports = function(count, delimiter) {
+    count = !Number.isInteger(count) || count < 1 ? 1 : count;
+    delimiter = !delimiter ? '' : delimiter;
+   
+    let min = 0;
+    let a = [];
+    for(var i = 0; i < count; i++) {
+        let max = list.length;
+        let index = rando(min, max);
+        if(list.length === 0)
+            list = getList();
+        let word = list.splice(index, 1);
+        a.push(word);
+    }
+    return a.join(delimiter);
+}
--- test.js
+++ test.js
@@ -1,6 +1,15 @@
-import test from 'ava';
-import m from './';
+const m = require('./')
 
-test(t => {
-	t.true(m().length > 1);
-});
+console.log(m());
+console.log(m(1));
+console.log(m(2));
+console.log(m(3));
+console.log(m(4));
+console.log(m(1,'-'));
+console.log(m(2,'!'));
+console.log(m(3,'-a-'));
+console.log(m(4,'3'));
+
+for(var i = 0; i < 30000; i++) {
+	console.log(m(4,'-'));
+}
